52313f9f16087396a94b46debd77f0a6c1af929a
Ernesto Alejo <ernestokarim@gmail.com>
2023-01-28 12:56:16 +0100

    Serialize civil dates.

--- datetime.go
+++ datetime.go
@@ -4,6 +4,8 @@ import (
 	"database/sql/driver"
 	"fmt"
 	"time"
+
+	"cloud.google.com/go/civil"
 )
 
 type Timestamp time.Time
@@ -12,9 +14,9 @@ func (t *Timestamp) Scan(value interface{}) error {
 	if value == nil {
 		return nil
 	}
-	switch value.(type) {
+	switch value := value.(type) {
 	case int64:
-		read := time.Unix(value.(int64), 0)
+		read := time.Unix(value, 0)
 		*t = Timestamp(read)
 		return nil
 	default:
@@ -31,4 +33,38 @@ func (t Timestamp) Value() (driver.Value, error) {
 
 func (t Timestamp) Time() time.Time {
 	return time.Time(t)
+}
+
+type Date civil.Date
+
+func (d *Date) Scan(value interface{}) error {
+	if value == nil {
+		return nil
+	}
+	switch value := value.(type) {
+	case string:
+		read, err := civil.ParseDate(value)
+		if err != nil {
+			return fmt.Errorf("sqltypes: cannot parse %q: %w", value, err)
+		}
+		*d = Date(read)
+		return nil
+	default:
+		return fmt.Errorf("sqltypes: unknown time type %T", value)
+	}
+}
+
+func (d Date) Value() (driver.Value, error) {
+	if d.Date().IsZero() {
+		return "", nil
+	}
+	return d.String(), nil
+}
+
+func (d Date) Date() civil.Date {
+	return civil.Date(d)
+}
+
+func (d Date) String() string {
+	return d.Date().String()
 }
--- datetime_test.go
+++ datetime_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 	"time"
 
+	"cloud.google.com/go/civil"
 	"github.com/stretchr/testify/require"
 )
 
@@ -73,4 +74,76 @@ func TestTimestampSaveLoad(t *testing.T) {
 	require.NoError(t, db.QueryRow("SELECT value_int FROM test_types WHERE name = ?", "foo").Scan(&other))
 
 	require.WithinDuration(t, other.Time(), time.Date(2018, 1, 2, 3, 4, 5, 0, time.UTC), time.Second)
+}
+
+func TestDateValue(t *testing.T) {
+	db := connectDB(t)
+	defer db.Close()
+
+	var date = Date(civil.Date{Year: 2018, Month: 1, Day: 2})
+	_, err := db.Exec("INSERT INTO test_types (name, value_int) VALUES (?, ?)", "foo", date)
+	require.NoError(t, err)
+
+	var val string
+	require.NoError(t, db.QueryRow("SELECT value_int FROM test_types WHERE name = ?", "foo").Scan(&val))
+
+	require.Equal(t, "2018-01-02", val)
+}
+
+func TestDateValueNil(t *testing.T) {
+	db := connectDB(t)
+	defer db.Close()
+
+	var date Date
+	_, err := db.Exec("INSERT INTO test_types (name, value_int) VALUES (?, ?)", "foo", date)
+	require.NoError(t, err)
+
+	var val string
+	require.NoError(t, db.QueryRow("SELECT value_int FROM test_types WHERE name = ?", "foo").Scan(&val))
+
+	require.Equal(t, "", val)
+}
+
+func TestDateScan(t *testing.T) {
+	db := connectDB(t)
+	defer db.Close()
+
+	_, err := db.Exec("INSERT INTO test_types (name, value_int) VALUES (?, ?)", "foo", "2018-01-02")
+	require.NoError(t, err)
+
+	var date Date
+	require.NoError(t, db.QueryRow("SELECT value_int FROM test_types WHERE name = ?", "foo").Scan(&date))
+
+	require.Equal(t, date.Date().Year, 2018)
+	require.Equal(t, date.Date().Month, time.January)
+	require.Equal(t, date.Date().Day, 2)
+}
+
+func TestDateScanNil(t *testing.T) {
+	db := connectDB(t)
+	defer db.Close()
+
+	_, err := db.Exec("INSERT INTO test_types (name, value_int) VALUES (?, ?)", "foo", nil)
+	require.NoError(t, err)
+
+	var date Date
+	require.NoError(t, db.QueryRow("SELECT value_int FROM test_types WHERE name = ?", "foo").Scan(&date))
+
+	require.True(t, date.Date().IsZero())
+}
+
+func TestDateSaveLoad(t *testing.T) {
+	db := connectDB(t)
+	defer db.Close()
+
+	var date = Date(civil.Date{Year: 2018, Month: 1, Day: 2})
+	_, err := db.Exec("INSERT INTO test_types (name, value_int) VALUES (?, ?)", "foo", date)
+	require.NoError(t, err)
+
+	var other Date
+	require.NoError(t, db.QueryRow("SELECT value_int FROM test_types WHERE name = ?", "foo").Scan(&other))
+
+	require.Equal(t, date.Date().Year, 2018)
+	require.Equal(t, date.Date().Month, time.January)
+	require.Equal(t, date.Date().Day, 2)
 }
